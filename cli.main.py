81b96c6f6f0e1486ad7a07704a7557c3bafeb3cd
Andrew Murphy <legionastartesofficial@gmail.com>
2026-03-27 00:52:40 +0900

    Add files via upload

---
+++ cli.main.py
@@ -0,0 +1,59 @@
+from storage import storage
+
+
+
+def menu_home():
+
+    name = input("Enter your name: ")
+    menu = (f"menu: hello {name} welcome to main menu, type '1', to enter registure as a new user, type '2' to login! type 'bye' to exit the menu! ")
+    print(menu)
+
+    while True:
+        user_input = input("type :")
+        if user_input == "bye":
+            print("menu: goodbye")
+            break
+
+
+        elif user_input == "1":
+            print("menu: registure selected")
+            name = input("make a new username: ")
+            password = input("Enter your new password: ")
+            print("thank you for signing up!")
+            storage[name] = password #this calls ot the storage !
+            print(storage)
+
+
+            #below is day 3
+            if name in storage:
+                if password == storage[name]:
+                    print("success")
+                else:
+                    print("not successful")
+            else:
+                print("error")
+
+
+        elif  user_input == "2":
+            print("menu: login selected")
+            name = input("username: ")
+            password = input("password: ")
+
+            storage[name] = password
+            #below is day 3
+            if name in storage:
+                if password == storage[name]:
+                    print("success")
+                else:
+                    print("not successful")
+            else:
+                print("error")
+
+
+        else:
+            print("menu: invalid option")
+
+
+
+menu_home()
+
