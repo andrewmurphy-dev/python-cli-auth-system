069931cbffb79ce173bbd41ffd12426252facec9
Andrew Murphy <legionastartesofficial@gmail.com>
2026-03-27 21:10:44 +0900

    Enhance user registration and login validation
    Added validation for username and password during registration and login processes.

--- cli.main.py
+++ cli.main.py
@@ -5,57 +5,101 @@ from storage import storage
 def menu_home():
 
     name = input("Enter your name: ")
-    menu = (f"menu: hello {name} welcome to main menu, type '1', to enter registure as a new user, type '2' to login! type 'bye' to exit the menu! ")
-    print(menu)
+
 
     while True:
+        menu = (
+            f"menu: hello {name} welcome to main menu, type '1', to enter registure as a new user, type '2' to login! type 'bye' to exit the menu! ")
+        print(menu)
         user_input = input("type :")
         if user_input == "bye":
             print("menu: goodbye")
             break
 
 
+
+
+
+        #sign up !
         elif user_input == "1":
             print("menu: registure selected")
+
+            #step 1
             name = input("make a new username: ")
+            #test 1
+            #user presses enter !
+           if name == "":
+               print("menu: error, try again!!!")
+
+
+
+            #test 2 registuring a user that already exists!
+
+            elif name in storage:
+            print("username is already taken! try again!")
+
+
+
+
             password = input("Enter your new password: ")
-            print("thank you for signing up!")
+            #test 1
+            if password == "":
+               print("menu: password cannot be blank!, try again!!!")
+               ret
+
+            #test 2
+
+            elif password in storage:
+                print("menu: password is already taken! try again!")
+
+             else:
+                 print("menu: thank you for signing up!")
+
+
+
             storage[name] = password #this calls ot the storage !
             print(storage)
 
+        else:
+            print("menu: not successful, try again!!!")
+
 
             #below is day 3
-            if name in storage:
-                if password == storage[name]:
-                    print("success")
-                else:
-                    print("not successful")
+        if name in storage:
+            if password == storage[name]:
+                    print("menu: success")
             else:
-                print("error")
+                print("menu: not successful, try again!!!")
 
 
+        #login
         elif  user_input == "2":
             print("menu: login selected")
             name = input("username: ")
+            #test 1 login already exists !
+            if name not in storage:
+                print("menu: username does not exist, try again!")
+                #test 2 menu cannot be blank!
+            elif name == "":
+                print("menu: username cannot be blank, try again!")
+
             password = input("password: ")
+            #test 1
+            if password == "":
+                print("menu: password cannot be blank, try again!")
+            elif password not in storage:
+                print("menu: password does not exist, try again!")
+
 
             storage[name] = password
             #below is day 3
             if name in storage:
                 if password == storage[name]:
                     print("success")
-                else:
-                    return menu_home() + "menu: not successful"
-            else:
-                return menu_home() + "menu: error"
 
 
-        else:
-            return menu_home() + "menu: invalid option"
 
-         
-         if user_input in ["7" , "abc", '']:
-         return f"{menu_home()} +  sorry your request is wrong , please try again"
+
 
 
 
