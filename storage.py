2704a6100a865ad750094896e6d0d8e709972c0e
Andrew Murphy <legionastartesofficial@gmail.com>
2026-03-27 21:11:29 +0900

    Add files via upload

---
+++ storage.py
@@ -0,0 +1,25 @@
+#storage does not need to be in the main.py cause its a global link
+#you must import the dictionary !
+
+#this is source code file
+#when the program runs , python creates the real dictionary in ram (tempory working copy)
+#if you want users to still exist after cloding the program, you need to save them somewhere !
+
+#text file
+#json file
+#database
+
+#this is pernament storage !
+
+
+#right now you only have memory !
+#never ever try to modify storage.py
+
+#json stores the stucture cleanly !
+
+#i want to test first ! this is called seed data
+
+storage = {"andrew": "barcelon"}
+
+
+
