cc522390ea437cfcbbcecf444934641e4e3edea7
Andrew Murphy <legionastartesofficial@gmail.com>
2026-03-28 23:47:54 +0900

    Add files via upload

---
+++ cli.main.py
@@ -0,0 +1,24 @@
+from auth import registure_user, login_user
+
+
+def menu_home():
+
+    name = input("Enter your name: ")
+
+
+    while True:
+        menu = (
+            f"menu: hello {name} welcome to main menu, type '1', to enter registure as a new user, type '2' to login! type 'bye' to exit the menu! ")
+        print(menu)
+        user_input = input("type :")
+        if user_input == "bye":
+            print("menu: goodbye")
+            break
+
+
+
+
+registure_user()
+login_user()
+menu_home()
+
