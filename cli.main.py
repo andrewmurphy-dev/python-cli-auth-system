c0f777025d21047c9f234b4adebfdfc81eda44a2
Andrew Murphy <legionastartesofficial@gmail.com>
2026-03-30 00:34:19 +0900

    Enhance menu with user selection for registration and login

--- cli.main.py
+++ cli.main.py
@@ -7,18 +7,27 @@ def menu_home():
 
 
     while True:
-        menu = (
-            f"menu: hello {name} welcome to main menu, type '1', to enter registure as a new user, type '2' to login! type 'bye' to exit the menu! ")
-        print(menu)
-        user_input = input("type :")
-        if user_input == "bye":
+      menu = (f"menu: hello {name} welcome to main menu, type '1', to enter registure as a new user, type '2' to login! type 'bye' to exit the menu! ")
+      print(menu)
+      user_input = input("type :")
+      if user_input == "bye":
             print("menu: goodbye")
             break
 
+      if user_input == "1":
+        print("menu: registure selected")
+        registure_user()
+
+      if user_input == "2":
+        print("menu: login selected")
+        login_user()
 
 
 
+
+
+
+menu_home()
 registure_user()
 login_user()
-menu_home()
 
