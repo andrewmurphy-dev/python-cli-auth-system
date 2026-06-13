3ccfda3d0b070c2a59275a77efcd49061ad102a7
Andrew Murphy <legionastartesofficial@gmail.com>
2026-06-13 20:34:17 +0900

    implement datetime, post_users, new_id functions

--- app/api/routers/users.py
+++ app/api/routers/users.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter 
+from datetime import datetime
+from zoneinfo import ZoneInfo 
 
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -7,8 +9,10 @@ router = APIRouter(prefix="/users", tags=["users"])
 
 #so we need fake data ! 
 fake_users = {
+    "id": 1,
     "name": "Andrew",
-    "password": 454563378
+    "password": 454563378,
+    "created_at": 
 }
 
 
@@ -16,3 +20,46 @@ fake_users = {
 @router.get("")
 def get_users():
     return fake_users 
+
+
+
+
+
+def new_user_id():
+    if not fake_users:
+        return 1 
+    
+    largest_user_id = 0 
+    
+    for user in fake_users:
+        if fake_users["id"] > largest_user_id:
+            largest_user_id = fake_users["id"]
+
+        
+    return largest_user_id + 1 
+
+
+
+
+
+JAPAN_TIMEZONE = ZoneInfo("Asia/Tokyo")
+
+def current_japan_time():
+    return datetime.now(JAPAN_TIMEZONE).isoformat()
+
+
+@router.post("")
+def create_user():
+    new_id = new_user_id()
+
+    new_user = {
+        "id": new_id,
+        "name": "andrew"
+        "password": "15267252"
+        "created_at": current_japan_time()
+
+    }
+
+    fake_users.appemd(new_user)
+
+    return new_user 
