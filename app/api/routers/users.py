fbf9097ad972347e9803dfcda8fc3aeb54c9f2a2
Andrew Murphy <legionastartesofficial@gmail.com>
2026-07-13 13:30:51 +0900

    implement sql-ruters in users

--- app/api/routers/users.py
+++ app/api/routers/users.py
@@ -14,8 +14,6 @@ class CreateStructure(BaseModel):
     password: str = Field(minlength=4)
 
 
-
-#so we need fake data ! 
 fake_users = [
     {
         "id": 1,
@@ -24,6 +22,9 @@ fake_users = [
         "created_at": "2026-06-16T00:00:00+09:00",
     }
 ]
+
+
+
 
 
 
@@ -48,46 +49,59 @@ def get_users(db: Session = Depends(get_db)):
 
 
 
-
-
-
-JAPAN_TIMEZONE = ZoneInfo("Asia/Tokyo")
-
-def current_japan_time():
-    return datetime.now(JAPAN_TIMEZONE).isoformat()
-
-
-
-
-
-
+#signup - user 
 
 @router.post("")
-def create_user(request: CreateStructure):
-    for user in fake_users:
-        if user["name"] == request.name:
-            raise HTTPException(status_code=409, detail="Username already exists")
+def create_user(request: CreateStructure, db: Session = Depends(get_db)):
+    result = db.execute(text("""INSERT INTO users (name, pasword)
+                             VALUES (:name, :password)
+                             RETURNING id, name, password, created_at,
+"""),
+{
+    "name": request.name,
+    "password": request.password,
+},
 
-    new_id = new_user_id()
+)
 
-    new_user = {
-        "id": new_id,
-        "name": request.name,
-        "password": request.password,
-        "created_at": current_japan_time()
 
+    db.commit()
+
+    row = result.mappings().one()
+
+    return {
+        "message": "Signed up successfully",
+        "user": {
+            "id": row["id"],
+            "name": row["name"],
+            "password": row["password"],
+            "created_at": int(row["created_at"].timestamp()),
+        },
     }
-
-    fake_users.append(new_user)
-
-    return {"message": "Signed up successfully!", "user": new_user}
 
 
 
 @router.post("/login")
-def login(request: CreateStructure):
-    for user in fake_users:
-        if user["name"] == request.name and user["password"] == request.password:
-            return {"message": "login successful!"}
+def login(request: CreateStructure, db: Session = Depends(get_db)):
+    result = db.execute(
+        text("""
+            SELECT id, name, password, created_at
+            FROM users
+            WHERE name = :name AND password = :password
+        """),
+        {
+            "name": request.name,
+            "password": request.password,
+        },
+    )
 
-    raise HTTPException(status_code=401, detail="Invalid username or password")
+    user = result.mappings().first()
+
+    if user is None:
+        raise HTTPException(status_code=401, detail="Invalid username or password")
+
+    return {"message": "login successful!"}
+
+
+
+
